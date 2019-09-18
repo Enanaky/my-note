cb015336de7ee28f8e68efad0886a3961b9ed8f5
Ignacio Petrone <ipetrone@Santiagos-MacBook-Pro.local>
2019-09-19 00:03:37 +0200

    deploying to ghpages

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -7,7 +7,7 @@ import Home from './Home';
 
 export default function Routes() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL + '/'}>
       <Switch>
         <Route exact path="/" component={SignIn} />
         <Route path="/signup" component={SignUp} />
